42448d75965b9b325ebde29e4a6790edf701ea0f
ryugi277 <rinayulius@gmail.com>
2022-06-05 14:59:25 +0700

    Initial Commit

---
+++ Collab/CollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollabApp.swift
+//  Collab
+//
+//  Created by Rina Yulius on 05/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
