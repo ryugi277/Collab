7fa2772bb5a0f24d9c2fa5fb5669d6ca83c5e7be
developedbyHaris <rajabulharis@gmail.com>
2022-06-05 15:30:09 +0700

    Add Haris 2 screen

---
+++ Collab/Haris2Screen.swift
@@ -0,0 +1,20 @@
+//
+//  Haris2Screen.swift
+//  Collab
+//
+//  Created by Rajabul Haris on 05/06/22.
+//
+
+import SwiftUI
+
+struct Haris2Screen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Haris2Screen_Previews: PreviewProvider {
+    static var previews: some View {
+        Haris2Screen()
+    }
+}
